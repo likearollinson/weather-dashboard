3d79698fe6379b0c05659265d625f6e433bee404
Brad Rollinson <BRollinson@shred-tech.com>
2021-07-15 09:47:35 -0400

    add some functionality in js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var APIkey = "&appid=a8036eed98a36f905e6ad2de800b6c58";
+var currentWeatherURL = "https//api.openweathermap.org/data/2.5/weather?q=";
+
+$("#searchBtn").click (function () {
+    var currentRequestURL = currentWeatherURL + $("#citySearch").val() + APIkey;
+    console.log();
+
+    fetch(currentRequestURL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function (data) {
+            conosole.log("Temp: " + data.user.login)
+        })
+});
