16e119e75cf757eb631009a7ee29546b47899961
Brad Rollinson <BRollinson@shred-tech.com>
2021-07-15 16:54:28 -0400

    add some images, add code and debug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var APIkey = "&appid=a8036eed98a36f905e6ad2de800b6c58";
+var APIkeyTwo = "&appid=b2fc22ce8308d8012b2c460f146793c8";
 var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
 var currentDay = moment().format("M/D/YY");
 
 $("#searchBtn").click (function () {
@@ -9,11 +11,18 @@ $("#searchBtn").click (function () {
             return response.json();
         })
         .then(function (data) {
-            console.log(data.main.temp + "°F")
             $("#currentTemp").text(data.main.temp + " °F")
             $("#currentWind").text(data.wind.speed + " MPH")
             $("#currentHumidity").text(data.main.humidity + "%")
-            $("#currentUV").text(data.main.temp + " °F")
+            // $("#currentUV").text(data.main.temp + " °F")
             $("#cityDate").text(data.name + " " + currentDay)
+        })
+    var forecastRequestURL = forecastURL + $("#citySearch").val() + "&units=imperial" + APIkey
+    fetch(forecastRequestURL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.list.main.temp)
         })
 });
