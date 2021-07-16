0dfae9720f46ace9a6ce2f141c728f541fec908f
Brad R <brad.w.rollinson@gmail.com>
2021-07-15 22:34:07 -0400

    add functionality where forecast data appears

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,6 @@ var forecastConditionArray = [];
 var forecastTempArray = [];
 var forecastWindArray = [];
 var forecastHumidityArray = [];
-var forecastDateArrayConvert = [];
 
 $("#searchBtn").click(function () {
     var currentRequestURL = currentWeatherURL + $("#citySearch").val() + "&units=imperial" + APIkey;
@@ -30,23 +29,40 @@ $("#searchBtn").click(function () {
         })
         .then(function (data) {
             for (i = 1; i < 6; i++) {
-                forecastDateArray.push(data.list[i].dt);
                 forecastConditionArray.push(data.list[i].weather[0].description);
                 forecastTempArray.push(data.list[i].main.temp);
                 forecastWindArray.push(data.list[i].wind.speed);
                 forecastHumidityArray.push(data.list[i].main.humidity);
-            }
-            for (var i = 0; i < 5; i++) {    
-                var date = new Date(forecastDateArray[i]*1000);
+                var date = new Date(data.list[i].dt*1000);
                 console.log(date.toLocaleDateString("en-US"));
-                forecastDateArrayConvert.push(date.toLocaleDateString("en-US"));
+                forecastDateArray.push(date.toLocaleDateString("en-US"));
             }
-            console.log(forecastDateArrayConvert)
-            $("#dateOne").text(forecastDateArrayConvert[0]);
-            $("#dateTwo").text(forecastDateArrayConvert[1]);
-            $("#dateThree").text(forecastDateArrayConvert[2]);
-            $("#dateFour").text(forecastDateArrayConvert[3]);
-            $("#dateFive").text(forecastDateArrayConvert[4]);
+            console.log(forecastDateArray)
+            // $("#dateOne").text(forecastDateArray[0]);
+            // $("#dateTwo").text(forecastDateArray[1]);
+            // $("#dateThree").text(forecastDateArray[2]);
+            // $("#dateFour").text(forecastDateArray[3]);
+            // $("#dateFive").text(forecastDateArray[4]);
+            // $("#conditionOne").text(forecastDateArray[0]);
+            // $("#conditionTwo").text(forecastDateArray[1]);
+            // $("#Three").text(forecastDateArray[2]);
+            // $("#dateFour").text(forecastDateArray[3]);
+            // $("#dateFive").text(forecastDateArray[4]);
+            $("#tempOne").text(forecastTempArray[0] + " °F");
+            $("#tempTwo").text(forecastTempArray[1] + " °F");
+            $("#tempThree").text(forecastTempArray[2] + " °F");
+            $("#tempFour").text(forecastTempArray[3] + " °F");
+            $("#tempFive").text(forecastTempArray[4] + " °F");
+            $("#windOne").text(forecastWindArray[0] + " MPH");
+            $("#windTwo").text(forecastWindArray[1] + " MPH");
+            $("#windThree").text(forecastWindArray[2]+ " MPH"); 
+            $("#windFour").text(forecastWindArray[3]+ " MPH");
+            $("#windFive").text(forecastWindArray[4]+ " MPH");
+            $("#humidityOne").text(forecastHumidityArray[0] + "%");
+            $("#humidityTwo").text(forecastHumidityArray[1] + "%");
+            $("#humidityThree").text(forecastHumidityArray[2] + "%");
+            $("#humidityFour").text(forecastHumidityArray[3] + "%");
+            $("#humidityFive").text(forecastHumidityArray[4] + "%");
         })
 
     // $("#dateOne").text(forecastDateArray[0]);
