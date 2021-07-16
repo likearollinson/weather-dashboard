456806c7b9ad814bfcbd25e6e12175ec45b25fe9
Brad R <brad.w.rollinson@gmail.com>
2021-07-15 21:26:31 -0400

    add first iteration of forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,17 @@ var APIkeyTwo = "&appid=b2fc22ce8308d8012b2c460f146793c8";
 var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
 var currentDay = moment().format("M/D/YY");
+var fiveDayDateArray = [];
+var fiveDayConditionArray = [];
+var fiveDayTempArray = [];
+var fiveDayWindArray = [];
+var fiveDayHumidityArray = [];
 
-$("#searchBtn").click (function () {
+
+$("#searchBtn").click(function () {
     var currentRequestURL = currentWeatherURL + $("#citySearch").val() + "&units=imperial" + APIkey;
     fetch(currentRequestURL)
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         })
         .then(function (data) {
@@ -19,10 +25,22 @@ $("#searchBtn").click (function () {
         })
     var forecastRequestURL = forecastURL + $("#citySearch").val() + "&units=imperial" + APIkey
     fetch(forecastRequestURL)
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data.list.main.temp)
+            for (i = 0; i < 5; i++) {
+                fiveDayDateArray.push(data.list[i].dt);
+                fiveDayConditionArray.push(data.list[i].weather[0].description);
+                fiveDayTempArray.push(data.list[i].main.temp);
+                fiveDayWindArray.push(data.list[i].wind.speed);
+                fiveDayHumidityArray.push(data.list[i].main.humidity);
+            }
+            console.log(fiveDayDateArray);
+            console.log(fiveDayConditionArray);
+            console.log(fiveDayTempArray);
+            console.log(fiveDayWindArray);
+            console.log(fiveDayHumidityArray);
         })
+
 });
