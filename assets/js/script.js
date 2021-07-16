8718918a19a12df57a983c65a64f0325ba7fc969
Brad R <brad.w.rollinson@gmail.com>
2021-07-15 22:09:57 -0400

    create arrays for forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,12 @@ var APIkeyTwo = "&appid=b2fc22ce8308d8012b2c460f146793c8";
 var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=";
 var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
 var currentDay = moment().format("M/D/YY");
-var fiveDayDateArray = [];
-var fiveDayConditionArray = [];
-var fiveDayTempArray = [];
-var fiveDayWindArray = [];
-var fiveDayHumidityArray = [];
-
+var forecastDateArray = [];
+var forecastConditionArray = [];
+var forecastTempArray = [];
+var forecastWindArray = [];
+var forecastHumidityArray = [];
+var forecastDateArrayConvert = [];
 
 $("#searchBtn").click(function () {
     var currentRequestURL = currentWeatherURL + $("#citySearch").val() + "&units=imperial" + APIkey;
@@ -29,18 +29,28 @@ $("#searchBtn").click(function () {
             return response.json();
         })
         .then(function (data) {
-            for (i = 0; i < 5; i++) {
-                fiveDayDateArray.push(data.list[i].dt);
-                fiveDayConditionArray.push(data.list[i].weather[0].description);
-                fiveDayTempArray.push(data.list[i].main.temp);
-                fiveDayWindArray.push(data.list[i].wind.speed);
-                fiveDayHumidityArray.push(data.list[i].main.humidity);
+            for (i = 1; i < 6; i++) {
+                forecastDateArray.push(data.list[i].dt);
+                forecastConditionArray.push(data.list[i].weather[0].description);
+                forecastTempArray.push(data.list[i].main.temp);
+                forecastWindArray.push(data.list[i].wind.speed);
+                forecastHumidityArray.push(data.list[i].main.humidity);
             }
-            console.log(fiveDayDateArray);
-            console.log(fiveDayConditionArray);
-            console.log(fiveDayTempArray);
-            console.log(fiveDayWindArray);
-            console.log(fiveDayHumidityArray);
+            for (var i = 0; i < 5; i++) {    
+                var date = new Date(forecastDateArray[i]*1000);
+                console.log(date.toLocaleDateString("en-US"));
+                forecastDateArrayConvert.push(date.toLocaleDateString("en-US"));
+            }
+            console.log(forecastDateArrayConvert)
+            $("#dateOne").text(forecastDateArrayConvert[0]);
+            $("#dateTwo").text(forecastDateArrayConvert[1]);
+            $("#dateThree").text(forecastDateArrayConvert[2]);
+            $("#dateFour").text(forecastDateArrayConvert[3]);
+            $("#dateFive").text(forecastDateArrayConvert[4]);
         })
+
+    // $("#dateOne").text(forecastDateArray[0]);
+    
+
 
 });
